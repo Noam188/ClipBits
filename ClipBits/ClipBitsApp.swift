e3aa70615cbb62019890ce3b28c6f5d961bcc696
Noam188 <Noamelya@icloud.com>
2021-09-01 23:30:13 -0400

    Initial Commit

---
+++ ClipBits/ClipBitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipBitsApp.swift
+//  ClipBits
+//
+//  Created by Noam Elyashiv on 01/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct ClipBitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipBits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClipBits
+//
+//  Created by Noam Elyashiv on 01/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
