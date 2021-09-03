ebeaa43802836d6a9fe84bd3d85e3d80701a0bcf
Noam188 <Noamelya@icloud.com>
2021-09-02 20:32:23 -0400

    Commit

--- ClipBits/ContentView.swift
+++ ClipBits/ContentView.swift
@@ -1,17 +1,70 @@
-//
-//  ContentView.swift
-//  ClipBits
-//
-//  Created by Noam Elyashiv on 01/09/2021.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var record = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Button(action:{
+                    record.toggle()
+                }) {
+                    Image(systemName: "mic.circle")
+                        .font(.system(size:45))
+                        .foregroundColor(.black)
+                }
+                Text("Press the mic, then select the slot you'd like to record on")
+                
+            }
+            HStack{
+                ButtonSlot()
+                ButtonSlot()
+                ButtonSlot()
+            }
+            HStack{
+                ButtonSlot()
+                ButtonSlot()
+                ButtonSlot()
+                
+            }
+            HStack{
+                ButtonSlot()
+                ButtonSlot()
+                ButtonSlot()
+                
+            }
+            HStack{
+                ButtonSlot()
+                ButtonSlot()
+                ButtonSlot()
+                
+            }
+            
+        }
+        .padding()
+        
     }
+}
+struct ButtonSlot: View {
+    @State private var isRecording = false
+    var contentView = ContentView()
+    var body: some View{
+        Button(action:{
+            if contentView.record == true{
+                isRecording.toggle()
+            }
+        }) {
+            ZStack{
+                Rectangle()
+                    .cornerRadius(20)
+                    .foregroundColor(.gray)
+                if isRecording == true{
+                    Image(systemName: "waveform.path")
+                }
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
